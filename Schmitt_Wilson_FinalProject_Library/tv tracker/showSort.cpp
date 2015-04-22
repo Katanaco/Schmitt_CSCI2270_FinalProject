97820fbc7e5f94372fd41e0ab7f4b8ef75167967
Katanaco <jonathan.schmitt@colorado.edu>
2015-04-22 15:15:55 -0600

    checking showSort.h and showSort.cpp

---
+++ Schmitt_Wilson_FinalProject_Library/tv tracker/showSort.cpp
@@ -0,0 +1,13 @@
+#include "showTraker.h"
+#include "showSort.h"
+using namespace std;
+
+show_Sort(show * ah){
+    archive_list_head = ah;
+}
+~show_Sort();
+show_ref * sort_by_rating(){
+
+
+}
+show_ref * sort_by_length();
---
+++ Schmitt_Wilson_FinalProject_Library/tv tracker/showSort.h
@@ -0,0 +1,37 @@
+#ifndef SHOWSORT_H_INCLUDED
+#define SHOWSORT_H_INCLUDED
+#include <iostream>
+
+struct show_ref{
+    show * ref_ptr;
+    show_ref * next;
+    show_ref * prev;
+
+    show_ref(){};
+
+    show_ref(show * rp, show_ref * n, show_ref p)
+    {
+        ref_ptr = rp;
+        next = n;
+        prev = p;
+    }
+
+};
+
+class show_Sort
+{
+    public:
+        show_Sort(show_ref *);
+        ~show_Sort();
+        show_ref * sort_by_rating();
+        show_ref * sort_by_length();
+
+    protected:
+    private:
+        show_ref * archive_list_head;
+
+
+};
+
+
+#endif // SHOWSORT_H_INCLUDED
